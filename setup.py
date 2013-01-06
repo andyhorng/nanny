b139d98605606419aeee9046eba53557449e5e7a
洪盟翔 <andy@eztable.com.tw>
2013-01-06 22:27:23 +0800

    init project

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Scaffolding',
+    version='0.1.0',
+    author='Andy',
+    author_email='andy@eztable.com.tw',
+    packages=find_packages(),
+    url='http://github.com/andyhorng',
+    license='LICENSE.txt',
+    description='',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "pystache",
+        "inflection"
+    ],
+)
